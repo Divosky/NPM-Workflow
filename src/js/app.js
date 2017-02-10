8b9dce39ee67388da53d7b701ee4bcb391962b22
Divosky <divoskyy@gmail.com>
2017-02-10 19:15:27 +0100

    Initial Commit

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+document.getElementById("header").innerHTML="masło";
+
+function idk() {
+  var first = "test",
+      second = "test2",
+      third = "test3";
+  document.getElementById("header").innerHTML=first;
+  console.log(second);
+  alert(third);
+}
